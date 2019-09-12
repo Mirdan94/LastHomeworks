7b33eeacc9273028ed6d50ce2ed97d8ac3484c0e
Mirdan Akinov <akinovmirdan@gmail.com>
2019-09-12 22:14:26 +0600

    Created onBind method

--- app/src/main/java/com/example/test/ViewHolderScreen.java
+++ app/src/main/java/com/example/test/ViewHolderScreen.java
@@ -1,13 +1,29 @@
 package com.example.test;
 
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ViewHolderScreen extends RecyclerView.ViewHolder {
 
+    TextView title;
+    CheckBox isDone;
+    TextView dates;
+
     public ViewHolderScreen (@NonNull View itemView) {
         super(itemView);
+
+        title = itemView.findViewById(R.id.task_title);
+        isDone = itemView.findViewById(R.id.task_isDone);
+        dates = itemView.findViewById(R.id.start_date);
+    }
+
+    public void onBind(Task task) {
+         title.setText(task.title);
+         isDone.setChecked(task.isDone);
+
     }
 }
