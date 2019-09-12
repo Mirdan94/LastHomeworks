b4929127bc3c83a31a176be17de97f3bcd707dae
Mirdan Akinov <akinovmirdan@gmail.com>
2019-09-12 18:47:48 +0600

    ViewHolder class with extend from Recycler View.ViewHolder

---
+++ app/src/main/java/com/example/test/ViewHolderScreen.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ViewHolderScreen extends RecyclerView.ViewHolder {
+
+    public ViewHolderScreen (@NonNull View itemView) {
+        super(itemView);
+    }
+}
