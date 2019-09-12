12a669b74e6c2e2dbb793c159189f8b2b8252714
Mirdan Akinov <akinovmirdan@gmail.com>
2019-09-12 20:37:46 +0600

    Task class created

---
+++ app/src/main/java/com/example/test/Task.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import java.util.Date;
+
+public class Task {
+
+    public String title;
+    public String description;
+    public Date startDate;
+    public Date endDate;
+    public boolean isDone;
+
+}
