91abfda2503e34ecbd9fb104ab4267203d92dc4c
Mirdan Akinov <akinovmirdan@gmail.com>
2019-09-14 00:20:15 +0600

    Adapter created

---
+++ app/src/main/java/com/example/test/TaskAdapter.java
@@ -0,0 +1,24 @@
+package com.example.test;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TaskAdapter extends RecyclerView.Adapter<ViewHolderScreen> {
+    @NonNull
+    @Override
+    public ViewHolderScreen onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderScreen holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
