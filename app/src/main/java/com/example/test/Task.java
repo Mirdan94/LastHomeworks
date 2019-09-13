54fc9e3e95e20f6d788f338f5a68909fa89ce6a2
Mirdan Akinov <akinovmirdan@gmail.com>
2019-09-13 21:49:54 +0600

    Task class filled in

--- app/src/main/java/com/example/test/Task.java
+++ app/src/main/java/com/example/test/Task.java
@@ -1,5 +1,7 @@
 package com.example.test;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Task {
@@ -9,5 +11,15 @@ public class Task {
     public Date startDate;
     public Date endDate;
     public boolean isDone;
+
+    public String dates () {
+
+        DateFormat format = new SimpleDateFormat();
+
+        String startDateStr = format.format(startDate);
+        String endDateStr = format.format(endDate);
+
+        return startDateStr + " - " + endDateStr;
+    }
 
 }
