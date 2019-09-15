797cd98042eb6c809a86283bb90bc86063b88e8e
Mirdan Akinov <akinovmirdan@gmail.com>
2019-09-16 01:43:13 +0600

    created DatePicker for startDate

--- app/src/main/java/com/example/test/FillDetailsActivity.java
+++ app/src/main/java/com/example/test/FillDetailsActivity.java
@@ -2,13 +2,48 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class FillDetailsActivity extends AppCompatActivity {
+
+    TextView tv_startDate;
+    Button bt_start;
+    Calendar c;
+    DatePickerDialog dpd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fill_details);
+
+        tv_startDate = (TextView) findViewById(R.id.tv_start);
+        bt_start = (Button) findViewById(R.id.bn_startDate);
+
+        bt_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                c = Calendar.getInstance();
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                int month = c.get(Calendar.MONTH);
+                int year = c.get(Calendar.YEAR);
+
+                dpd = new DatePickerDialog(FillDetailsActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int Year, int Month, int Day) {
+
+                        tv_startDate.setText(Day + "/" + Month + "/" + Year);
+                    }
+                }, day, month, year);
+                dpd.show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -4,10 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void openDetails (Task task) {
+    private void openDetails(Task task) {
 
         Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
         intent.putExtra(DetailsActivity.DETAILS_KEY, task);
