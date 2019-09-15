affcf57c22b2acfc8ac3dd35fb86386c6451011e
Mirdan Akinov <akinovmirdan@gmail.com>
2019-09-15 20:22:57 +0600

    Adapter methods filled in

--- app/src/main/java/com/example/test/TaskAdapter.java
+++ app/src/main/java/com/example/test/TaskAdapter.java
@@ -1,24 +1,35 @@
 package com.example.test;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class TaskAdapter extends RecyclerView.Adapter<ViewHolderScreen> {
+
+    ArrayList<Task> tasks;
+
     @NonNull
     @Override
     public ViewHolderScreen onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.view_holder_screen, parent, false);
+        return new ViewHolderScreen(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolderScreen holder, int position) {
+        holder.onBind(tasks.get(position));
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return tasks.size();
     }
 }
